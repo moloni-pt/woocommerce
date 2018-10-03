188a76b469df07df61aed286a757f06461970ed3
Nunong21 <nuno@moloni.com>
2018-10-03 10:52:57 +0100

    Start files

---
+++ includes/classes/moloni.class.php
@@ -0,0 +1,25 @@
+<?php
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+/**
+ * Description of moloni
+ *
+ * @author Nuno
+ */
+class Moloni
+{
+    public function __construct()
+    {
+        
+    }   
+    
+    public function run($param)
+    {
+        echo "Hello World!";
+    }
+    //put your code here
+}
---
+++ moloni.php
@@ -0,0 +1,36 @@
+<?php
+/*
+  Plugin Name:  Moloni
+  Plugin URI:   https://plugins.moloni.com/woocommerce
+  Description:  Send your orders automatically to your Moloni invoice software
+  Version:      0.0.1
+  Author:       Moloni.com
+  Author URI:   https://moloni.com
+  License:      GPL2
+  License URI:  https://www.gnu.org/licenses/gpl-2.0.html
+ */
+
+define('MOLONI_VERSION', '0.0.1');
+
+function moloni_install()
+{
+    
+}
+
+function moloni_deactivate()
+{
+    
+}
+
+register_activation_hook(__FILE__, 'moloni_install');
+register_deactivation_hook(__FILE__, 'moloni_deactivate');
+
+require plugin_dir_path(__FILE__) . 'includes/classes/moloni.class.php';
+
+function moloniRun()
+{
+    $moloni = new Moloni();
+    $moloni->run();
+}
+
+moloniRun();
