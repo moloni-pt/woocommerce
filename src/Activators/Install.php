347848f7f295bd551abbb8df25dba9575fe5b9a2
Fábio Correia <fabio.correia@moloni.com>
2022-08-17 17:02:53 +0100

    Fix query syntax

--- src/Activators/Install.php
+++ src/Activators/Install.php
@@ -10,7 +10,7 @@ class Install
      * Install Settings table
      * Start sync crons
      */
-    public static function run()
+    public static function run(): void
     {
         if (!function_exists('curl_version')) {
             deactivate_plugins(plugin_basename(__FILE__));
@@ -29,7 +29,7 @@ class Install
     /**
      * Create API connection table
      */
-    private static function createTables()
+    private static function createTables(): void
     {
         global $wpdb;
         $wpdb->query(
@@ -58,30 +58,29 @@ class Install
     /**
      * Create Moloni account settings
      */
-    private static function insertSettings()
+    private static function insertSettings(): void
     {
         global $wpdb;
 
         $wpdb->query("INSERT INTO `" . $wpdb->prefix . "moloni_api_config`(config, description) 
                         VALUES('document_set_id', 'Escolha uma Série de Documentos para melhor organização'),
-                        VALUES('exemption_reason', 'Escolha uma Isenção de Impostos para os produtos que não têm impostos'),
-                        VALUES('exemption_reason_shipping', 'Escolha uma Isenção de Impostos para os portes que não têmimpostos'),
-                        VALUES('payment_method', 'Escolha um metodo de pagamento por defeito'),
-                        VALUES('measure_unit', 'Escolha a unidade de medida a usar'),
-                        VALUES('maturity_date', 'Prazo de Pagamento'),
-                        VALUES('document_status', 'Escolha o estado do documento (fechado ou em rascunho)'),
-                        VALUES('document_type', 'Escolha o tipo de documentos que deseja emitir'),
-                        VALUES('client_prefix', 'Prefixo da referência do cliente'),
-                        VALUES('product_prefix', 'Prefixo da referência do produto'),
-                        VALUES('use_moloni_product_details', 'Usar os detalhes do artigo já existente no Moloni'),
-                        VALUES('update_final_consumer', 'Actualizar consumidor final'),
-                        VALUES('shipping_info', 'Informação de envio'),
-                        VALUES('vat_field', 'Número de contribuinte'),
-                        VALUES('email_send', 'Enviar email'),
-                        VALUES('moloni_stock_sync', 'Sincronizar Stocks'),
-                        VALUES('moloni_product_sync', 'Inserir Artigos'),
-                        VALUES('moloni_product_sync_update', 'Actualizar Artigos')
+                        ('exemption_reason', 'Escolha uma Isenção de Impostos para os produtos que não têm impostos'),
+                        ('exemption_reason_shipping', 'Escolha uma Isenção de Impostos para os portes que não têmimpostos'),
+                        ('payment_method', 'Escolha um metodo de pagamento por defeito'),
+                        ('measure_unit', 'Escolha a unidade de medida a usar'),
+                        ('maturity_date', 'Prazo de Pagamento'),
+                        ('document_status', 'Escolha o estado do documento (fechado ou em rascunho)'),
+                        ('document_type', 'Escolha o tipo de documentos que deseja emitir'),
+                        ('client_prefix', 'Prefixo da referência do cliente'),
+                        ('product_prefix', 'Prefixo da referência do produto'),
+                        ('use_moloni_product_details', 'Usar os detalhes do artigo já existente no Moloni'),
+                        ('update_final_consumer', 'Actualizar consumidor final'),
+                        ('shipping_info', 'Informação de envio'),
+                        ('vat_field', 'Número de contribuinte'),
+                        ('email_send', 'Enviar email'),
+                        ('moloni_stock_sync', 'Sincronizar Stocks'),
+                        ('moloni_product_sync', 'Inserir Artigos'),
+                        ('moloni_product_sync_update', 'Actualizar Artigos')
                         ");
     }
-
 }
