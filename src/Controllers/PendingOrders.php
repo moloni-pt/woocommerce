cc926d5d5d845b6b841f8da3eb0655cd455ce4a5
Fábio Correia <fabio.correia@moloni.com>
2022-10-20 15:41:56 +0100

    New filter
    Added new filter that allows users to change arguments of pending orders query.

--- src/Controllers/PendingOrders.php
+++ src/Controllers/PendingOrders.php
@@ -41,6 +41,7 @@ class PendingOrders
             ],
         ];
 
+        $args = apply_filters('moloni_before_pending_orders_fetch', $args);
 
         $query = new WP_Query($args);
         self::$totalPages = $query->max_num_pages;
