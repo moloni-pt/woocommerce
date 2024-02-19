3be93f50accc1d0fd3a07d2276514ccddc09f840
Fábio Correia <fabio.correia@moloni.com>
2024-02-19 09:53:00 +0000

    Cache bust

--- src/Scripts/Enqueue.php
+++ src/Scripts/Enqueue.php
@@ -12,7 +12,7 @@ class Enqueue
 {
     public static function defines() {
         if (isset($_REQUEST['page']) && !wp_doing_ajax() && sanitize_text_field($_REQUEST['page']) === 'moloni') {
-            $ver = '4.6';
+            $ver = '4.7';
 
             wp_enqueue_style('moloni-styles', plugins_url('assets/css/moloni.min.css', MOLONI_PLUGIN_FILE), [], $ver);
             wp_enqueue_script('moloni-scripts', plugins_url('assets/js/moloni.min.js', MOLONI_PLUGIN_FILE), [], $ver);
